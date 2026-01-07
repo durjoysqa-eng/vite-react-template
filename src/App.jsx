1da4447bf7d5de014d95797fd5ea7baf38058bf8
durjoysqa-eng <durjoysqa@gmail.com>
2026-01-07 12:48:54 +0600

    deploying cloudflare 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './component/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Home></Home>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
